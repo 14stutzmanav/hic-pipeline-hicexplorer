611ad05c2d21b1b2ab2d554b79afd2aebc3b85f3
Alex Stutzman <14stutzmanav@gmail.com>
2022-09-27 09:17:05 -0400

    Updated submission scripts to point to correct slurmConfig.json

--- dryrunSnakemake.sh
+++ dryrunSnakemake.sh
@@ -15,4 +15,4 @@ fi
 echo ${snakeFile}
 
 #snakemake --rerun-incomplete --printshellcmds -s ${snakeFile} --latency-wait 60 --cluster "sbatch -J {rule} -o ./Err_Out/slurm-%j.out -e ./Err_Out/slurm-%j.err -N1 --time 2:00:00 --mem=16G" --jobs 500
-snakemake --dry-run --rerun-incomplete --cluster-config clusterConfig/slurmConfig.json --cores 1 --printshellcmds -s ${snakeFile} --rerun-incomplete --latency-wait 200 --cluster  "sbatch -J {rule} -o ./Err_Out/slurm-%j.out -e ./Err_Out/slurm-%j.err -N1 -n {cluster.threads} --time {cluster.time} --mem={cluster.mem}" --jobs 500
+snakemake --dry-run --rerun-incomplete --cluster-config slurmConfig.json --cores 1 --printshellcmds -s ${snakeFile} --rerun-incomplete --latency-wait 200 --cluster  "sbatch -J {rule} -o ./Err_Out/slurm-%j.out -e ./Err_Out/slurm-%j.err -N1 -n {cluster.threads} --time {cluster.time} --mem={cluster.mem}" --jobs 500
